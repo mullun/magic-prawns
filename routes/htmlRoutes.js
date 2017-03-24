04e92e132b943f18374a3b7b8eb173e09e14c2f8
aaronjaymus <aaronjaymus@gmail.com>
2017-03-23 21:14:09 -0400

    Started with HTML routes and API routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,9 +5,30 @@ var db = require("../models")
 
 module.exports = function(app){
 	app.get("/", function(req, res){
-		var hbsObject = {
-			Dish: data
-		};
-		res.render("index", hbsObject);
+	    db.Dish.findAll({})
+	    .then(function(dbDish) {
+	    //sort by rating	
+	      res.json(dbDish);
+	    });
 	});
+
+
+	app.get("/api", function(req, res) {
+    	// db.User.create({
+    	// 	user_name: "tester"
+    	// }).then(function(dbUser){
+    	// 	res.json(dbUser);
+    	// });
+
+   //  	db.Dish.create({
+   //  		dish_name: "test food",
+   //  		restaurant: "test resto",
+   //  		rating: 3,
+   //  		zip_code: 27516,
+   //  		cuisine: "test cuisine",
+   //  		userId: 1
+   //  	}).then(function(dbPost) {
+   //    		res.json(dbPost);
+   //  	});
+  	// });
 };
